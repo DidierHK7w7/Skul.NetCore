70cd2dbae7394b8a532fb4c68601cd5fce40e303
Didier Hobak <dual.didier.hobak@gmail.com>
2022-05-04 17:09:26 -0500

    Clase timbre

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
 ﻿using System;
-namespace Skul{
+namespace Skul
+{
+    public class School
+    {
+        public string name;
+        public string address;
+        public int foundationYear;
+        string ceo;
+
+        public void Ring()
+        {
+            Console.Beep(1000, 3000);     //Pitido de 10000 hz a 3000ms
+        }
+    }
+
     public class Program{
-        static void Main(){
+        static void Main()
+        {
+            var mySchool = new School();
+            mySchool.name = "Bocchi";
+            mySchool.address = "Groove Street, Los Santos";
+            mySchool.foundationYear = 2012;
+
+            Console.WriteLine("Ringing");
+            mySchool.Ring();
             
         }
     }
