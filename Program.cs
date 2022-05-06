fe5a07a173897b3eb6b49465c181772ce2aa28cf
Didier Hobak <dual.didier.hobak@gmail.com>
2022-05-06 10:48:55 -0500

    Lección 17: Formas de recorrer arreglos

--- Program.cs
+++ Program.cs
@@ -3,33 +3,55 @@ using Skul.Entities;
 
 namespace Skul
 {
-    public class Program{
+    public class Program
+    {
         static void Main()
         {
             var school = new School("Platzi Academy", 2012, SchoolTypes.Elementary,
             city:"Bogota", country:"Colombia");
 
-            var course1 = new Course()
-            {
-                Name = "101"
-            };
+            var courseArray = new Course[3];
+            courseArray[0] = new Course(){Name = "101"};
+            courseArray[1] = new Course(){Name = "201"};
+            courseArray[2] = new Course(){Name = "301"};
 
-            var course2 = new Course()
-            {
-                Name = "201"
-            };
-
-            var course3 = new Course()
-            {
-                Name = "301"
-            };
 
             Console.WriteLine(school);
+            Console.WriteLine("---------------------");
 
-            Console.WriteLine("-----------------------\n"+course1.Name+","+course1.UniqueId);
-            Console.WriteLine(course2.Name+","+course2.UniqueId);  
-            //Console.WriteLine($"{course3.Name}, {course3.UniqueId}");  
-            Console.WriteLine(course3);  
+            PrintCourses(courseArray);
+        }
+        
+        private static void PrintCourses(Course[] courseArray)
+        {
+            //Formas de imprimir un array
+
+            foreach (var item in courseArray)
+            {
+                    Console.WriteLine($"Nombre: {item.Name},    id: {item.UniqueId}");
+            }
+
+
+            /*int count = 0;
+            while (count < courseArray.Length)
+            {
+                Console.WriteLine($"Nombre: {courseArray[count].Name}, id: {courseArray[count].UniqueId}");
+                count++;
+            }*/
+
+
+            /*int count = 0;
+            do
+            {
+                Console.WriteLine($"Nombre: {courseArray[count].Name}, id: {courseArray[count].UniqueId}");
+                count++;
+            } while (count++ < courseArray.Length);*/
+
+
+            /*for (int i = 0; i < courseArray.Length; i++)
+            {
+                Console.WriteLine($"Nombre: {courseArray[i].Name}, id: {courseArray[i].UniqueId}");
+            }*/
         }
     }
 }
