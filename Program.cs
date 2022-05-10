053818616c2ff03bf7c3d8bbd69215770b529793
Didier Hobak <dual.didier.hobak@gmail.com>
2022-05-10 11:55:29 -0500

    U3. Lección 3. Polimorfismo

--- Program.cs
+++ Program.cs
@@ -16,6 +16,35 @@ namespace Skul
             Printer.WriteTitle("Welcome to School");
             Printer.Ring();
             PrintSchoolCourses(engine.School);
+
+            Printer.DrawLine(20);
+            Printer.DrawLine(20);
+            Printer.DrawLine(20);
+            Printer.WriteTitle("Pruebas de polimorfismo");
+
+            var studentTest = new Student{Name="Sayo"};
+            SchoolBaseObject ob = studentTest;
+            //ob.Name = "Sayo";       //ob toma Name de Student por uqe este hereda de SchoolBaseObject
+            Printer.WriteTitle("Student");
+            WriteLine($"Student:{studentTest.Name}");
+            WriteLine($"Student:{studentTest.UniqueId}");
+            WriteLine($"Student:{studentTest.GetType()}");      //GetTyoe obtiene el tipo de objeto
+
+            Printer.WriteTitle("SchoolBaseObject");
+            WriteLine($"Student:{ob.Name}");
+            WriteLine($"Student:{ob.UniqueId}");
+            WriteLine($"Student:{ob.GetType()}");
+
+            var objDummy = new SchoolBaseObject(){Name="Ran Mitake"};
+            Printer.WriteTitle("objDummy");
+            WriteLine($"Student:{objDummy.Name}");
+            WriteLine($"Student:{objDummy.UniqueId}");
+            WriteLine($"Student:{objDummy.GetType()}");
+
+            studentTest = (Student)ob;
+            WriteLine($"Student:{studentTest.Name}");
+            WriteLine($"Student:{studentTest.UniqueId}");
+            WriteLine($"Student:{studentTest.GetType()}");
         }
 
         private static void PrintSchoolCourses(School school)
